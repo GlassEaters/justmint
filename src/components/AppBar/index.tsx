b61060f43ee971fe9dc3d098a3f3aa1dda52744c
Lawrence Wu <larry.wu@solana.com>
2022-02-12 21:41:47 -0800

    Add nav to metadata signing page

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-import { Modal } from 'antd';
+import { Button, Modal } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
 import { useWallet } from '@solana/wallet-adapter-react';
 
@@ -115,8 +115,29 @@ export const MobileNavbar = () => {
   )
 }
 
+const penSvg = (filled: boolean) => {
+  const props = filled
+    ? { fill: 'white' }
+    : {
+      fill: 'none',
+      stroke: 'white',
+      strokeWidth: '20px',
+    };
+  return (
+    <svg width="20" height="20" viewBox="0 0 512 512" {...props} >
+      <rect x="129.177" y="254.498" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -161.9856 228.3968)" width="131.06" height="110.469"/>
+      <path d="M92.531,347.45l-71.281,42.037L0,504.437l114.951-21.249l42.037-71.282L92.531,347.45z M80.187,447.865
+        c-6.519,6.52-17.091,6.52-23.611,0s-6.52-17.091,0-23.611c6.52-6.52,17.091-6.52,23.611,0
+        C86.707,430.774,86.707,441.345,80.187,447.865z"/>
+      <path d="M468.181,128.393l9.648-9.648c25.401-25.401,25.401-66.73,0-92.131c-25.401-25.401-66.73-25.401-92.131,0L218.591,193.718
+        l92.132,92.132l133.389-133.389l19.751,19.751L300.806,335.269l24.068,24.068L512,172.212L468.181,128.393z"/>
+    </svg>
+  )
+}
+
 export const AppBar = () => {
   const { connected } = useWallet();
+  const location = useLocation();
   return (
     <>
       <MobileNavbar />
@@ -135,6 +156,11 @@ export const AppBar = () => {
                 showAddress={true}
                 iconSize={24}
               />
+                <Button className="wallet-key">
+                  <Link to="/sign">
+                    {penSvg(location.pathname === "/sign")}
+                  </Link>
+                </Button>
               <Cog />
             </>
           )}
